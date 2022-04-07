9f14ea1ef0de36a0bc5662387d3ca6140f672648
surendrachaurasia200 <surendrachaurasia200@gmail.com>
2022-04-07 21:36:02 +0530

    Updated JS.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let questionArray = ["Enter a verb", "Enter a noun", "Enter a Adjective"];
 
+let questionCounter=0;
+
 for (let i=questions; i>=0; i--){
+    questionCounter +=1;
     console.log(i);
 }
+
