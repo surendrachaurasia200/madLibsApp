1538787f003c839e991ac7e3a7458142ce9dd4f1
surendrachaurasia200 <surendrachaurasia200@gmail.com>
2022-04-08 13:20:11 +0530

    complete madlibs

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-let questionArray = ["Enter a verb", "Enter a noun", "Enter a Adjective"];
+"use strict"
 
+let numberofQuestions = 3;
 let questionCounter=0;
 
-for (let i=questions; i>=0; i--){
+let questionArray = ["Enter a Adjective", "Enter a verb not ending in -ing", "Enter a noun"];
+let userInputs = [];
+
+for (let i=numberofQuestions; i>0; i--){
+    /*console.log(questionCounter);
+    console.log(i); */
+    console.log(questionArray[questionCounter] + `...(${numberofQuestions} questions left)`);
+    
+    userInputs.push(
+        prompt(questionArray[questionCounter] + `... (${numberofQuestions} questions left)`));
+        
+    console.log(userInputs);
     questionCounter +=1;
-    console.log(i);
+    numberofQuestions -=1;
 }
 
+let originalStory = `There once was a ${userInputs[0]} programmer who wanted to use JavaScript to ${userInputs[1]} the ${userInputs[2]}.`;
+
+alert(
+    "All done! Ready for your totally-accurate, not-at-all confusing mad libs??"
+  );
+
+console.log(originalStory);
+
+document.write(originalStory);
+
+/* There once was a ${userInputs[0]} programmer who wanted to use JavaScript to ${userInputs[1]} the ${userInputs[2]}. */
